1520046ee44d63ebece691c838763f1c74abf905
Evoluwilll <142166104+Evoluwilll@users.noreply.github.com>
2024-03-13 07:54:02 -0300

    atualizações do aluara midi

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
-document.querySelector('.tecla_pom');
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
 
-document.querySelector('#som_tecla_pom').play();
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//para
+for ( let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;//template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code);
+
+        if (evento.code === 'Space') {
+          tecla.classList.add('ativa');
+        }
+       
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
